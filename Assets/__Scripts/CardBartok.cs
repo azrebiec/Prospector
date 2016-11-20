1671c5fc4734f6ae27e6581558b7d46f640c73ac
azrebiec <andrew.zrebiec@rutgers.edu>
2016-11-20 11:43:40 -0500

    added code to handle new sorting layers

--- Assets/__Scripts/CardBartok.cs
+++ Assets/__Scripts/CardBartok.cs
@@ -30,6 +30,9 @@ public class CardBartok : Card {
 	public List<Quaternion>		bezierRots;
 	public float				timeStart, timeDuration; //declares 2 fields
 
+	public int eventualSortOrder;
+	public string eventualSortLayer;
+
 	//when the card is done moving, it will call reportFinishTo.SendMessage()
 	public GameObject			reportFinishTo = null;
 
@@ -118,6 +121,17 @@ public class CardBartok : Card {
 				transform.localPosition = pos;
 				Quaternion rotQ = Utils.Bezier(uC, bezierRots);
 				transform.rotation = rotQ;
+
+				if (u>0.5f && spriteRenderers[0].sortingOrder != eventualSortOrder) {
+					// Jump to the proper sort order
+					SetSortOrder(eventualSortOrder);
+				}
+				if (u>0.75f && spriteRenderers[0].sortingLayerName !=
+					eventualSortLayer) {
+					// Jump to the proper sort layer
+					SetSortingLayerName(eventualSortLayer);
+				}
+
 			}
 			break;
 		}
--- Assets/__Scripts/Player.cs
+++ Assets/__Scripts/Player.cs
@@ -44,6 +44,9 @@ public class Player {
 			// every turn, it isn't a problem.
 		}
 
+		eCB.SetSortingLayerName("10"); // This sorts the moving card to the top
+		eCB.eventualSortLayer = handSlotDef.layerName;
+
 		FanHand();
 		return( eCB );
 	}
@@ -105,7 +108,8 @@ public class Player {
 			hand[i].faceUp = (type == PlayerType.human);
 
 			// Set the SortOrder of the cards so that they overlap properly
-			hand[i].SetSortOrder(i*4);
+			hand[i].eventualSortOrder = i*4;
+			//hand[i].SetSortOrder(i*4);
 		}
 	}
 }
